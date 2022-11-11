3b1c13f007ef355ab9ad861be5bc840b696107f8
Batuhan Simsek <simsekbatu2004@gmail.com>
2022-11-11 16:07:06 +0300

    Add StudyExecutor.java and First spring project

--- java/study-thread-programming/src/com/example/StudyExecuters.java
+++ java/study-thread-programming/src/com/example/StudyExecuters.java
@@ -11,7 +11,7 @@ public class StudyExecuters {
         ExecutorService es = Executors.newFixedThreadPool(cores);
 
         for (int i = 0; i < 1000; i++) {
-            es.submit(() -> System.out.println("Hello World"));
+            es.submit(() -> System.out.println("Hello World "));
         }
 
         es.shutdown();
